7f248548c44c314cd351b0f6c69f96270ae53230
deepak <deepakmk.ec@gmail.com>
2019-08-25 11:33:38 +0530

    Initial Commit

---
+++ MoneyManagerTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoneyManagerTest
+//
+//  Created by Deepak on 8/25/19.
+//  Copyright © 2019 Deepak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
