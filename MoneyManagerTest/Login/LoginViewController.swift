dac2b9de1a9ad8cc942e82c4dafb05d4c112a897
deepak <deepakmk.ec@gmail.com>
2019-08-27 03:21:29 +0530

    Added Login screen

---
+++ MoneyManagerTest/Login/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  MoneyManagerTest
+//
+//  Created by Deepak on 8/27/19.
+//  Copyright © 2019 Deepak. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
